6cab8f7ae9ac0b367db524881dd7e3f242cda42f
Trey Batey <ekt.batey@gmail.com>
2015-03-04 14:41:02 -0800

    Update testpost

---
+++ posts_knit-Rmd-to-md.R
@@ -0,0 +1,14 @@
+myjekyllsite = c("http://bateyt.github.io/skeptistats/")
+
+KnitPost <- function(input, base.url = myjekyllsite) {
+  require(knitr)
+  opts_knit$set(base.url = base.url)
+  fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+  opts_chunk$set(fig.path = fig.path)
+  opts_chunk$set(fig.cap = "center")
+  render_jekyll()
+  knit(input, envir = parent.frame())
+}
+
+# be sure to edit file name for the original RMarkdown file
+KnitPost("~/skeptistats/_assets/rmds/2015-03-04-testpost.Rmd")
